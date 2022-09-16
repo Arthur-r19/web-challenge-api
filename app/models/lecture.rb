be2ddca894f8e2cd6cba28d26445ffef6c63ab99
ArthurB <arthur.rlima19@gmail.com>
2022-09-16 16:38:34 -0300

    add duration only integer greater than zero validation

--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -2,5 +2,5 @@ class Lecture < ApplicationRecord
   belongs_to :track, optional: true
 
   validates :name, presence: true
-  validates :duration, presence: true
+  validates :duration, presence: true, numericality: { only_integer: true, greater_than: 0 }
 end
