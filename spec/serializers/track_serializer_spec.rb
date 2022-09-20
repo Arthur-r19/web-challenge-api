13d78da97b5846f48a0959837841bbfc28a629c2
ArthurB <arthur.rlima19@gmail.com>
2022-09-20 00:14:30 -0300

    add track serializer tests

--- spec/serializers/track_serializer_spec.rb
+++ spec/serializers/track_serializer_spec.rb
@@ -1,18 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe TrackSerializer, type: :serializer do
-  let(:competition) { create(:competition) }
-  let(:serializer) { described_class.new(competition) }
+  let(:track) { create(:track) }
+  let(:serializer) { described_class.new(track) }
   let(:serialization) { ActiveModelSerializers::Adapter.create(serializer) }
 
-  let(:subject) { JSON.parse(serialization.to_json)['competition'] }
+  let(:subject) { JSON.parse(serialization.to_json) }
 
   it 'when all params matches' do
-    expected_params = {
-      'id' => competition.id,
-      'name' => competition.name
-    }
-    expect(subject).to eql(expected_params)
+    expected_params = { 'id' => track.id,
+                        'lectures' => track.lectures }
+    expect(subject['id'] == expected_params['id']).to be_truthy
+    expect(subject['lectures'] == expected_params['lectures']).to be_truthy
   end
 end
 
