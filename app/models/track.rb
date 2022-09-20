79c55724b05d5cb797631316e479a0f29dd207a5
ArthurB <arthur.rlima19@gmail.com>
2022-09-20 02:22:39 -0300

    fix track method

--- app/models/track.rb
+++ app/models/track.rb
@@ -4,8 +4,8 @@ class Track < ApplicationRecord
   def self.delete_all_tracks
     # delete all lunch/networking lectures and all tracks
     Track.all.each do |t|
-      Lecture.find(t.lunch_id)&.destroy unless t.lunch_id.nil?
-      Lecture.find(t.networking_id)&.destroy unless t.networking_id.nil?
+      Lecture.find_by(id: t.lunch_id)&.destroy unless t.lunch_id.nil?
+      Lecture.find_by(id: t.networking_id)&.destroy unless t.networking_id.nil?
       t.destroy
     end
   end
